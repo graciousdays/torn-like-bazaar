9d3bec901f85fee6d6ebfd586a7a10032498b265
Atreides Tir <gracious.days@gmail.com>
2025-07-17 00:14:09 +0700

    fix: remove unused React import for CI linting

--- src/features/market/MarketPage.tsx
+++ src/features/market/MarketPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchItems, addToCart } from "./marketSlice";
 import type { RootState } from "../../app/store";
